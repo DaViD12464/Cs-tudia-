e5cb1bd7367e1569dd4eb9711f93d28a87073204
Dawid <dawidczernik@interia.pl>
2024-06-01 15:08:26 +0200

    Zadania_7

---
+++ Zadania_7/zad1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int calc(char *line) {
+    int length = strlen(line);
+    int firstval, lastval;
+
+    for(int i=0; i<length; i++) {
+        if (line[i] >= '0' && line[i] <= '9'){
+            firstval = line[i] - '0';
+            break;
+        }
+    }
+
+    for(int i= length; i>=0;i--) {
+        if(line[i] >= '0' && line[i] <= '9') {
+            lastval = line[i] - '0';
+            break;
+        }
+    }
+    return firstval *10 +lastval;
+}
+
+int main() {
+    int sum = 0;
+
+    FILE *file = fopen("input.txt", "r");
+
+    if (file == NULL) {
+        printf("Cannot find file with provided name!\n");
+        return 1;
+    }
+
+    char line[100];
+
+    while (fgets(line,sizeof(line),file)!= NULL) {
+        sum += calc(line);
+    }
+
+    fclose(file);
+    printf("Sum of all energetic values: %d\n", sum);
+    return 0;
+}
